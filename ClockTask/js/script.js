0c0dc6257a2fea71c3731b35aef738b38d02436b
TrollGG174 <56648952+TrollGG174@users.noreply.github.com>
2021-02-11 17:36:53 +0500

    Update script.js

--- ClockTask/js/script.js
+++ ClockTask/js/script.js
@@ -24,10 +24,6 @@ let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
 let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
 let secPosition = sec * 360 / 60;
 
-if(secPosition > 0){
-  document.write("Время отчислений");
-}
-
 function runTheClock() {
 
   hrPosition += 3/360;
