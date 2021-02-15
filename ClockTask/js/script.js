59b854ba6b91748e48f2d1f0d09e07c33a2092e7
TrollGG174 <56648952+TrollGG174@users.noreply.github.com>
2021-02-15 10:58:33 +0500

    Update script.js

--- ClockTask/js/script.js
+++ ClockTask/js/script.js
@@ -24,21 +24,18 @@ let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
 let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
 let secPosition = sec * 360 / 60;
 
+if((hrPosition % 360) >= (9 * 360 / 12) && (hrPosition % 360) <= (9 * 360 / 12) + ((1 / 60) * 360 / 12)) 
+    alert("Время отчислений");
+
 function runTheClock() {
 
   hrPosition += 3/360;
   minPosition += 6/60;
   secPosition += 6;
-  
-  if((hrPosition % 360) >= (9 * 360 / 12) && (hrPosition % 360) <= (9 * 360 / 12) + ((1 / 60) * 360 / 12)) 
-    alert("Время отчислений");
 
   HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
   MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
   SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
-  
-  if((hrPosition % 360) >= (9 * 360 / 12) && (hrPosition % 360) <= (9 * 360 / 12) + ((1 / 60) * 360 / 12)) 
-    alert("Время отчислений");
 }
 
 // run runTheClock function every second
