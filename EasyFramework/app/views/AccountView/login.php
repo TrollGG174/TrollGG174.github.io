982d422f278c3c2b66012cd0b2ddda846b9187ca
TrollGG174 <56648952+TrollGG174@users.noreply.github.com>
2022-04-30 22:04:21 +0500

    Delete EasyFramework directory

--- EasyFramework/app/views/AccountView/login.php
+++
@@ -1,8 +0,0 @@
-<h3>Вход</h3>
-<form action="/account/login" method="post">
-    <p>Логин</p>
-    <p><input type="text" name="login"></p>
-    <p>Пароль</p>
-    <p><input type="text" name="pass"></p>
-    <p><button type="submit" name="enter">Войти</button></p>
-</form>
--- EasyFramework/app/views/MainView/index.php
+++
@@ -1,6 +0,0 @@
-<p>Главная страница</p>
-<?php foreach ($smth as $key => $value): ?>
-    <h2><p><?php echo $value['title']; ?></p></h2>
-    <p><?php echo $value['smthtext']; ?></p>
-    <hr>
-<?php  endforeach; ?>
